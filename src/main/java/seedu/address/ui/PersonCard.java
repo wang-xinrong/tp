4b4c7c620542e7c4067f1dcd072b51f2e17c3dc4
Wang Xinrong <wangxinrong8@gmail.com>
2024-03-08 15:46:53 +0800

    Add tutorial 2 part 2

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -41,6 +41,9 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    @FXML
+    private Label remark;
+
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
